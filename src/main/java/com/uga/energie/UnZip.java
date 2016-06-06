5c2577e9041363ac66a2d216d5178f71f32026cc
dustbeen43@gmail.com <0953Seb>
2016-06-06 17:05:49 +0200

    Modification unzip : desarchivage au niveau 2 + delete old files

--- src/main/java/com/uga/energie/UnZip.java
+++ src/main/java/com/uga/energie/UnZip.java
@@ -15,26 +15,24 @@ public class UnZip {
 
     public static void main(String[] args) {
         UnZip unZip = new UnZip();
+
+        /*
         System.out.println("Nombre d'argument : " + args.length);
         System.out.println("les arguments : ");
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
+        */
 
         if (args.length == 2) {
-            unZip.unZipIt(args[0], args[1]);
+            unZip.unZipAllFiles(args[0], args[1]);
         } else {
             String INPUT_ZIP_FILE = "C:\\Users\\Artybloc\\Dropbox\\Master\\M2S2\\LinlyBDD\\data.zip";
             String OUTPUT_FOLDER = "D:\\Temp";
-            unZip.unZipIt(INPUT_ZIP_FILE, OUTPUT_FOLDER);
+            unZip.unZipAllFiles(INPUT_ZIP_FILE, OUTPUT_FOLDER);
         }
     }
 
-    /**
-     * Unzip it
-     *
-     * @param zipFile input zip file
-     */
     public void unZipIt(String zipFile, String outputFolder) {
 
         byte[] buffer = new byte[1024];
@@ -48,8 +46,7 @@ public class UnZip {
             }
 
             //get the zip file content
-            ZipInputStream zis =
-                    new ZipInputStream(new FileInputStream(zipFile));
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
             //get the zipped file list entry
             ZipEntry ze = zis.getNextEntry();
 
@@ -85,4 +82,92 @@ public class UnZip {
         }
     }
 
+    public void UnZipLevelOne(String INPUT_ZIP_FILE, String OUTPUT_FOLDER){
+        unZipIt(INPUT_ZIP_FILE, OUTPUT_FOLDER);
+    }
+
+    public void UnZipLevelTwo(String OUTPUT_FOLDER){
+        File folder = new File(OUTPUT_FOLDER + "\\data");
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                System.out.println(file.getAbsolutePath());
+                unZipIt(file.getAbsolutePath(), OUTPUT_FOLDER);
+            }
+        }
+    }
+
+    public void deleteOldZip(String OUTPUT_FOLDER){
+        File directory = new File(OUTPUT_FOLDER + "\\data");
+
+        //make sure directory exists
+        if(!directory.exists()){
+
+            System.out.println("Directory does not exist.");
+            System.exit(0);
+
+        }else{
+
+            try{
+
+                delete(directory);
+
+            }catch(IOException e){
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+
+    }
+
+    public void unZipAllFiles(String INPUT_ZIP_FILE, String OUTPUT_FOLDER){
+        System.out.println("-------------------------------- NIVEAU 1 ------------------------------------");
+        UnZipLevelOne(INPUT_ZIP_FILE, OUTPUT_FOLDER);
+        System.out.println("-------------------------------- NIVEAU 2 ------------------------------------");
+        UnZipLevelTwo(OUTPUT_FOLDER);
+        System.out.println("---------------------------- DELETE OLD FILES --------------------------------");
+        deleteOldZip(OUTPUT_FOLDER);
+    }
+
+    public static void delete(File file)
+            throws IOException{
+
+        if(file.isDirectory()){
+
+            //directory is empty, then delete it
+            if(file.list().length==0){
+
+                file.delete();
+                System.out.println("Directory is deleted : "
+                        + file.getAbsolutePath());
+
+            }else{
+
+                //list all the directory contents
+                String files[] = file.list();
+
+                for (String temp : files) {
+                    //construct the file structure
+                    File fileDelete = new File(file, temp);
+
+                    //recursive delete
+                    delete(fileDelete);
+                }
+
+                //check the directory again, if empty then delete it
+                if(file.list().length==0){
+                    file.delete();
+                    System.out.println("Directory is deleted : "
+                            + file.getAbsolutePath());
+                }
+            }
+
+        }else{
+            //if file, then delete it
+            file.delete();
+            System.out.println("File is deleted : " + file.getAbsolutePath());
+        }
+    }
 }
+
