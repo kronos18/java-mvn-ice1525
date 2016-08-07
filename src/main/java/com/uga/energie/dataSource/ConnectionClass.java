c251738efe976d91aa0ae3f83bea31b808dd550c
kronos18 <ivan.raficoff@gmail.com>
2016-08-07 15:13:58 +0200

    Correction du null pointer exception pour le fichier db.properties

--- src/main/java/com/uga/energie/dataSource/ConnectionClass.java
+++ src/main/java/com/uga/energie/dataSource/ConnectionClass.java
@@ -3,17 +3,12 @@
  */
 package com.uga.energie.dataSource;
 
-import org.apache.maven.wagon.resource.Resource;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-
-import static org.apache.maven.wagon.providers.ssh.ScpHelper.getResource;
-import static org.apache.maven.wagon.providers.ssh.ScpHelper.getResourceFilename;
 
 
 public class ConnectionClass {
@@ -22,37 +17,30 @@ public class ConnectionClass {
 
 
     private ConnectionClass() {
+        if (null == dataSource) {
+            configureDataSource();
+        }
+    }
+
+    public static Connection getDataSource() {
+        if (null == dataSource) {
+            configureDataSource();
+        }
+        return dataSource;
+    }
+
+    private static void configureDataSource() {
         Properties props = new Properties();
         FileInputStream fileProperty = null;
         try {
             fileProperty = new FileInputStream("src/main/resources/db.properties");
             props.load(fileProperty);
             dataSource = DriverManager.getConnection(props.getProperty("postgresql.url"), props.getProperty("postgresql.username"), props.getProperty("postgresql.password"));
-
+            dataSource.setAutoCommit(false);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public static Connection getDataSource() {
-        if (null == dataSource) {
-            Properties props = new Properties();
-            FileInputStream fileProperty = null;
-
-            try {
-                fileProperty = new FileInputStream("src/main/resources/db.properties");
-                props.load(fileProperty);
-                dataSource = DriverManager.getConnection(props.getProperty("postgresql.url"), props.getProperty("postgresql.username"), props.getProperty("postgresql.password"));
-                dataSource.setAutoCommit(false);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return dataSource;
     }
 }
